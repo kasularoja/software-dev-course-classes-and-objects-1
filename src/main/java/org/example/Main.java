46bd6732c94b2e802edf6fb6e3cf1b01d6843df8
kasularoja <kasularoja67@gmail.com>
2025-06-09 09:39:13 -0500

    classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,21 @@
+
 package org.example;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
+        //calculator operations
         System.out.print("Enter the first number: ");
         int a = scanner.nextInt();
         System.out.print("Enter the second number: ");
         int b = scanner.nextInt();
+        System.out.println("The sum is " + Calculator.add(a, b));
+        System.out.println("The difference is " + Calculator.subtract(a, b));
+        System.out.println("The product is " + Calculator.multiply(a, b));
+        System.out.println("The quotient is " + Calculator.divide(a, b));
 
-        // Use the static methods you wrote in the Calculator class
-        // to assign proper values to the four variables below
-        // (instead of 0)
-        int sum = 0;
-        int difference = 0;
-        int product = 0;
-        int quotient = 0;
-
-        System.out.println("The sum is " + sum);
-        System.out.println("The difference is " + difference);
-        System.out.println("The product is " + product);
-        System.out.println("The quotient is " + quotient);
     }
-
 }
